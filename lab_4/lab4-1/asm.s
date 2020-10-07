a13b846bbfece0438cebac8c7402fccb84330dc8
lee-sj <201302386@hufs.ac.kr>
2020-10-07 18:52:58 +0900

    lab 4-1
    it gt / ite gt 의 if-then 의 작동 원리가 이해되지않음

--- lab_4/lab4-1/asm.s
+++ lab_4/lab4-1/asm.s
@@ -13,35 +13,58 @@ __iar_program_start
         // CODE32
 
 main    
-        mov r0, #5
-        mov r1, #4
-        mov r2, #6
-        
-        ldr r10, =0x20000000
-        
-        stmia r10, {r0-r2}
+        // 1. Store 10 integer values at memory[0x20000000]~[0x20000024]
 
-        mov r0, #0
-        mov r1, #0
-        mov r2, #0
+        // Assign 10 integer values into r0 to r9 using MOV instruction
+        mov r0, #6
+        mov r1, #5
+        mov r2, #7
+        mov r3, #4
+        mov r4, #8
+        mov r5, #3
+        mov r6, #9
+        mov r7, #2
+        mov r8, #10
+        mov r9, #1
+
+        // Let r10 point to memory address of 0x20000000 using MOV instruction 
+        mov r10, #0x20000000
+        // Store multile values of r0 to r9 into memory address pointed by r10 STM instruction
+        stmia r10, {r0-r9}
         
+        // 2. Branch with link to Search subroutine
         BL search+1
 
+
+        // 4. Returning from the Search subroutine Save the maximum value to memory[0x20000028]
+        ldr r12, =0x20000028    // Let r12 point to memory address of 0x20000028 using LDR instruction
+        str r0, [r12]       // Store maximum value(r0) to memory(r12)
+
+        nop
+        nop
+
+
+        // 3. In Search subroutine
 search
-        mov r10, #0x20000000
+        // Load (first and second)two data saved at memory address pointed by r10 into r0 with increment of address by 4 using LDR instruction
         ldr r0, [r10], #4
-        
-loop
-        
         ldr r1, [r10], #4
-        cmp r0, r1
-        ite gt
-        movgt r3, r0
-        movle r4, r1
-        cmp r1, #0
-        bne loop
+        
+        // Move into Loop 
+loop    
+        // 3-1. Find maximum value from 10 integer values in that memory
+        cmp r0, r1              // Compare r0, r1 
+        ite gt                  // If r0 is great than r1
+        movgt r0, r0           // Biger Num Assign r0 to r0
+        movle r0, r1           // Smaller Num Assign r1 to r
+        ldr r1, [r10], #4       // Load (next) data saved at memory address potinted by r10 into r1 with increment of address by 4 using LDR instruction
+        // 3-2 Save the maximum value( Assigned by loop into r11 ) to r0
+        cmp r1, #0              // Compare r1, 0 ( means - next data is exist)
+        bne loop                // if not branch to loop (this line make loop)
 
-
+        // 3-3 return 
+        mov pc, lr
+        
         NOP
         
         END
