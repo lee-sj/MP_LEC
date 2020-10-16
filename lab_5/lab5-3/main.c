5416852a7bf9b0e410dfb834266bb1ca71764ac0
lee-sj <201302386@hufs.ac.kr>
2020-10-16 15:44:47 +0900

    lab5-2 fix  실행 & lab5-3 set

--- lab_5/lab5-3/main.c
+++ lab_5/lab5-3/main.c
@@ -10,8 +10,10 @@ int main(void)
   
   GPIO_InitTypeDef GPIO_InitStructure;
 
+  // unsigned int LED_data = 0x0080;    
   
   unsigned int switch_value = 0x0;
+  unsigned char FND_DATA_TBL[]={0x3F,0X06,0X5B,0X4F,0X66,0X6D,0X7C,0X07,0X7F,0X67,0X77,0X7C,0X39,0X5E,0X79,0X71,0X08,0X80};
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOC, ENABLE);
   
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0| GPIO_Pin_1 | GPIO_Pin_2| GPIO_Pin_3| 
