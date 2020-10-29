466d3a0f984b0ed1f6150df08329d51cfe95825c
lee-sj <201302386@hufs.ac.kr>
2020-10-29 15:37:47 +0900

    lab5-3 fix

--- lab_5/lab5-3/main.c
+++ lab_5/lab5-3/main.c
@@ -2,36 +2,42 @@
 #include "stm32f10x_lib.h"
 #include "System_func.h"
 
-void Delay(vu32 nCount);
+void Delay(vu32 nCount){
+  for(;nCount !=0; nCount--);
+}
 
 int main(void)
 {
-  Init_STM32F103();
   
   GPIO_InitTypeDef GPIO_InitStructure;
 
-  
-  unsigned int switch_value = 0x0;
   unsigned char FND_DATA_TBL[]={0x3F,0X06,0X5B,0X4F,0X66,0X6D,0X7C,0X07,0X7F,0X67,0X77,0X7C,0X39,0X5E,0X79,0X71,0X08,0X80};
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOC, ENABLE);
-  
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0| GPIO_Pin_1 | GPIO_Pin_2| GPIO_Pin_3| 
-  GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+  unsigned char cnt=0,i;
+
+  unsigned int Pin_info = PIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7; 
+
+  Init_STM32F103();
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
   
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0| GPIO_Pin_1 | GPIO_Pin_2| GPIO_Pin_3| 
-  GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  GPIO_ResetBits(GPIOA, 0x0F);
 
   while (1)  {
 
-    switch_value = GPIO_ReadInputData(GPIOC);
-    GPIO_Write(GPIOA, switch_value);
-  
+    GPIO_Write(GPIOC, FND_DATA_TBL[0]);
+    Delay(0xFFFF);
+
   }
+  
   
 }
 
